800a41183df1fe2259fc6005a2167268a8904d2c
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-10-14 23:10:24 +0200

    added rtl8812au driver

--- modules/hardware/rtl8821au.nix
+++ modules/hardware/rtl8821au.nix
@@ -2,6 +2,7 @@
 {
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8821au
+    rtl8812au
   ];
-  boot.initrd.kernelModules = [ "8821au" ];
+  boot.initrd.kernelModules = [ "8821au" "8812au" ];
 }
