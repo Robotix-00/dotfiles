8b133f5c56a41822cb4c136de2c566c825c6d2ab
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-11-13 22:29:07 +0100

    replaced xmobar 'volume' with 'alsa'

--- dotfiles/xmonad/xmobar/xmobar.hs
+++ dotfiles/xmonad/xmobar/xmobar.hs
@@ -7,7 +7,7 @@ Config {
 
   sepChar = "%",
   alignSep = "}{",
-  template = "<box type=Bottom width=2 mb=2 color=#268bd2>%XMonadLog%</box> | %multicpu% %coretemp% | <box type=Bottom width=2 mb=2 color=#268bd2>%memory% + %swap%</box> | %dynnetwork% }{<action=`sh .config/xmonad/xmobar/refreshBackground.sh`>[~]</action> %uptime% %default:Master% <box type=Bottom width=2 mb=2 color=#ee9a00><fc=#ee9a00>%date%</fc></box> <box type=Bottom width=2 mb=2 color=#268bd2>%uname%</box> ",
+  template = "<box type=Bottom width=2 mb=2 color=#268bd2>%XMonadLog%</box> | %multicpu% %coretemp% | <box type=Bottom width=2 mb=2 color=#268bd2>%memory% + %swap%</box> | %dynnetwork% }{<action=`sh .config/xmonad/xmobar/refreshBackground.sh`>[~]</action> %uptime% %alsa:default:Master% <box type=Bottom width=2 mb=2 color=#ee9a00><fc=#ee9a00>%date%</fc></box> <box type=Bottom width=2 mb=2 color=#268bd2>%uname%</box> ",
 
   commands =
     [ Run DynNetwork
@@ -38,9 +38,9 @@ Config {
       [ "-t", "<box type=Bottom width=2 mb=2 color=#268bd2>Uptime: <days>d <hours>h <minutes>m</box>"
       ] 10
     --! crashing xmobar on startup
-    , Run Volume "default" "Master"
+    , Run Alsa "default" "Master"
       [ "-t", "<box type=Bottom width=2 mb=2 color=#268bd2>Vol <action=`amixer set Master 10%-`>-</action> <volumebar> <action=`amixer set Master 10%+`>+</action> <action=`amixer set Master toggle`><status></action></box>"
-      ] 10
+      ]
     -- , Run Com ".config/xmonad/xmobar/soundmixer.sh" [] "audio" 10
     , Run Memory ["-t","Mem: <usedbar>"] 10
     , Run Swap ["-t", "<usedratio>%"] 10
