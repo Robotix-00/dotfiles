43b60b5d98cfd15f7bcdf43faea57c0ae5d86ef0
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-11-18 17:28:06 +0100

    tmux config

--- modules/shells/tmux.nix
+++ modules/shells/tmux.nix
@@ -3,8 +3,16 @@
   programs.tmux = {
     enable = true;
     baseIndex = 1;
+    shortcut = "a";
 
+    keyMode = "vi";
     terminal = "tmux-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      nord      # theme
+      tmux-fzf  # fuzzy finder
+      vim-tmux-navigator
+    ];
   };
 }
 
