252351cb01bf4bd2bc50985b5231fe36e3614956
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-11-19 22:46:32 +0100

    simplified audio config -> accidently fixed xmobar crashing

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,25 +1,8 @@
 { pkgs, ... }:
 {
-  # Enable sound with pipewire.
-  sound.enable = false;
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
+  sound.enable = true;
+  hardware.pulseaudio = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    support32Bit = true;
   };
-
-
-  environment.systemPackages = with pkgs; [
-    alsa-utils
-  ];
-
-  systemd.user.services.pipewire-pulse.path = [ pkgs.pulseaudio ];
 }
