e58484a8a50fd26b48da9529620785d14a45b4ce
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-11-24 18:36:47 +0100

    line displacement keybinds

--- packages/vim/nvim/lua/robotix/core/keybinds.lua
+++ packages/vim/nvim/lua/robotix/core/keybinds.lua
@@ -14,6 +14,13 @@ vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close tab
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>") -- prev tab
 
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
 -- plugins - Telescope
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
