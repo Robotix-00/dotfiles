69b9e08e2e5fc0c4e2563c0e59d3332a577b5db2
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-12-21 15:33:25 +0100

    changed harpoon shortcuts

--- packages/vim/nvim/lua/robotix/plugins/harpoon.lua
+++ packages/vim/nvim/lua/robotix/plugins/harpoon.lua
@@ -6,8 +6,8 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-c>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-f>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-g>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-i>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-i>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
 
 vim.keymap.set("n", "<C-p>", function() ui.nav_next() end)
 vim.keymap.set("n", "<C-o>", function() ui.nav_prev() end)
