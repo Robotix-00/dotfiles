ccceb4965ae83e5002e1abaedf5e3f1e711911a4
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-12-29 13:36:38 +0100

    disabled uninstalled lsp config

--- packages/vim/nvim/lua/robotix/plugins/lsp/init.lua
+++ packages/vim/nvim/lua/robotix/plugins/lsp/init.lua
@@ -36,26 +36,26 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
+-- require('lspconfig')['pyright'].setup{
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+-- }
 
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
-}
+-- require('lspconfig')['rust_analyzer'].setup{
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+--     -- Server-specific settings...
+--     settings = {
+      -- ["rust-analyzer"] = {}
+    -- }
+-- }
 
 require('lspconfig')['rnix'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
 }
 
-require('lspconfig')['hls'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
+-- require('lspconfig')['hls'].setup {
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+-- }
